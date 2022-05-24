db7b0043b4944629bf6e7e3870a2745c77c76840
Juan Ramirez <juanpram2000@gmail.com>
2022-05-23 23:32:31 -0700

    Creating an initial folder structure for a full Stack project. Planning to use React + Flask With a possible TailwindCSS integration. This is just an initial file structure
    Features to be add:
        A database
        Github branches for development
        A CSS library
        Publications to Google Cloud
        A domain for the project
        Test Driven Development
        Other developers
        Developers notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+function test() {
+  axios.get('/home').then((response) =>{
+    console.log(response.data.home);
+    }, (error) => {
+      console.log(error);
+    }); 
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>
+      Hello World;
+    </h1>
+
+    <button onClick={test}>
+      Test this button
+    </button>
+    </>
   );
 }
 
